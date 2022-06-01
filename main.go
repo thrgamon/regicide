1474b7abe135386c300340131b01578456b3d0b9
Tom Gamon <tom@tomgamon.com>
2022-06-01 17:06:05 +1000

    Cleanup formatting

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"regexp"
 	"strings"
-  "flag"
 
 	"github.com/fatih/color"
 	"github.com/jroimartin/gocui"
@@ -20,22 +20,22 @@ var debugMode bool
 var multiLine bool
 
 func init() {
-  flag.BoolVar(&debugMode, "debug", false, "Run in debug mode")
-  flag.BoolVar(&multiLine, "multiline", false, "Run against multiple lines of text")
+	flag.BoolVar(&debugMode, "debug", false, "Run in debug mode")
+	flag.BoolVar(&multiLine, "multiline", false, "Run against multiple lines of text")
 }
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  if debugMode {
-    f, err := os.OpenFile("debug.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
-    if err != nil {
-        log.Fatalf("error opening file: %v", err)
-    }
-    defer f.Close()
+	if debugMode {
+		f, err := os.OpenFile("debug.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("error opening file: %v", err)
+		}
+		defer f.Close()
 
-    fileLog.SetOutput(f)
-  }
+		fileLog.SetOutput(f)
+	}
 
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
@@ -54,67 +54,65 @@ func main() {
 		log.Panicln(err)
 	}
 
-
-  go updater(g)
-
+	go updater(g)
 
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
 		log.Panicln(err)
 	}
 
-  rv, _ := g.View("regex")
-  line := rv.ViewBuffer()
+	rv, _ := g.View("regex")
+	line := rv.ViewBuffer()
 	g.Close()
-  println(line)
+	println(line)
 }
 
 func setView(g *gocui.Gui, v *gocui.View) error {
-  g.SetCurrentView(v.Name())
-  return nil
+	g.SetCurrentView(v.Name())
+	return nil
 }
 
 func updater(g *gocui.Gui) {
-  for {
-    select {
-  case reg := <-regex:
-    g.Update(func(g *gocui.Gui) error {
-      v,err := g.View("results")
-      rv,err := g.View("regex")
+	for {
+		select {
+		case reg := <-regex:
+			g.Update(func(g *gocui.Gui) error {
+				v, err := g.View("results")
+				rv, err := g.View("regex")
 
-      if err != nil {
-        return err
-      }
+				if err != nil {
+					return err
+				}
 
-      if debugMode {
-        fileLog.Print(reg)
-      }
+				if debugMode {
+					fileLog.Print(reg)
+				}
 
-      v.Clear()
-      reRaw := strings.Replace(rv.ViewBuffer(), "\n", "", 1)
-      if reRaw == "" {
-        fmt.Fprint(v, userString)
-        return nil
-      }
+				v.Clear()
+				reRaw := strings.Replace(rv.ViewBuffer(), "\n", "", 1)
+				if reRaw == "" {
+					fmt.Fprint(v, userString)
+					return nil
+				}
 
-      re, err := regexp.Compile(reRaw)
-      if err != nil {
-        fmt.Fprint(v, err.Error())
-      } else {
-        matches := ReturnsMatch(re, userString)
-        if true {
-            PrintResultsMultiline(v,userString, matches)
-        } else {
-          for _, result := range matches {
-            PrintResults(v,userString, result)
-          }
-        }
-      }
+				re, err := regexp.Compile(reRaw)
+				if err != nil {
+					fmt.Fprint(v, err.Error())
+				} else {
+					matches := ReturnsMatch(re, userString)
+					if true {
+						PrintResultsMultiline(v, userString, matches)
+					} else {
+						for _, result := range matches {
+							PrintResults(v, userString, result)
+						}
+					}
+				}
 
-      return nil
-    })
-  }
+				return nil
+			})
+		}
 
-  }
+	}
 }
 
 func layout(g *gocui.Gui) error {
@@ -129,11 +127,11 @@ func layout(g *gocui.Gui) error {
 		g.SetCurrentView("regex")
 	}
 
-	if vr, err := g.SetView("results", 1, 4, maxX-2, maxY - 2); err != nil {
+	if vr, err := g.SetView("results", 1, 4, maxX-2, maxY-2); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-    fmt.Fprint(vr, userString)
+		fmt.Fprint(vr, userString)
 	}
 
 	return nil
@@ -157,46 +155,46 @@ func simpleEditor(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
 		v.MoveCursor(1, 0, false)
 	}
 
-  go func() {regex <- ""}()
+	go func() { regex <- "" }()
 }
 
 func quit(g *gocui.Gui, v *gocui.View) error {
-  close(regex)
+	close(regex)
 	return gocui.ErrQuit
 }
 
 type Colorer func(a ...interface{}) string
 
 func PrintResultsMultiline(w io.Writer, userString string, matches [][]int) {
-    // Setup the color function
-    blue := color.New(color.BgBlue).SprintFunc()
-    red := color.New(color.BgRed).SprintFunc()
+	// Setup the color function
+	blue := color.New(color.BgBlue).SprintFunc()
+	red := color.New(color.BgRed).SprintFunc()
 
-  for index, char := range userString {
-    var match bool
-    var colorer Colorer
+	for index, char := range userString {
+		var match bool
+		var colorer Colorer
 
-    for mi, matchIndex := range matches {
-      if mi % 2 == 0 {
-        colorer = red
-      } else {
-        colorer = blue
-      }
+		for mi, matchIndex := range matches {
+			if mi%2 == 0 {
+				colorer = red
+			} else {
+				colorer = blue
+			}
 
-      if index >= matchIndex[0] && index < matchIndex[1] {
-        match = true
-      }
-      if match == true {
-        break
-      }
-    }
+			if index >= matchIndex[0] && index < matchIndex[1] {
+				match = true
+			}
+			if match == true {
+				break
+			}
+		}
 
-    if match == true {
-      fmt.Fprintf(w, "%s", colorer(string(char)))
-    } else {
-      fmt.Fprintf(w, "%s", string(char))
-    }
-  }
+		if match == true {
+			fmt.Fprintf(w, "%s", colorer(string(char)))
+		} else {
+			fmt.Fprintf(w, "%s", string(char))
+		}
+	}
 }
 
 func PrintResults(w io.Writer, userString string, matchIndex []int) {
